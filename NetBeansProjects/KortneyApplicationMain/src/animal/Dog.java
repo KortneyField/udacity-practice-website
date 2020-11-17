bace1bb8d9199fa64bd7f678ac3885fbe675bf41
KortneyField <kortneyfield@gmail.com>
2020-11-17 10:39:51 -0600

    new dog inventory project

---
+++ NetBeansProjects/KortneyApplicationMain/src/animal/Dog.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package animal;
+
+/**
+ *
+ * @author kortneyfield
+ */
+public class Dog {
+    
+    //Declair fields
+    private String breed; 
+    
+    //Declair methods
+    public String getBreed() {
+     return breed;    
+    }
+}
